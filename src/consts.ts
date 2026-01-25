9edd59d9ddf7a130d620ffa410878bf1cf8d57c9
Jabriel <jabrielah@outlook.com>
2026-01-25 22:09:55 +0800

    feat: alt title for header with animation

--- src/consts.ts
+++ src/consts.ts
@@ -2,7 +2,13 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "Spacerad.io";
-export const SITE_TITLE_ALT = ["太空来电", "宇宙频道", "星际广播"] as const;
+export const SITE_TITLE_ALT = [
+    "太空来电",
+    "宇宙频道",
+    "星际广播",
+    "I'm Jabriel",
+    "智人大冲浪",
+] as const;
 export const SITE_DESCRIPTION = "Personal blog and digital garden";
 export const SITE_SOCIAL_IMAGE = "/og-image.png";
 export const DEFAULT_POST_SOCIAL_IMAGE = "/og-image.png";
@@ -16,12 +22,18 @@ export const SOCIAL_LINKS = {
     rss: "/rss.xml",
 } as const;
 
+type NavLink = {
+    href: string;
+    label: string;
+    drawer_only?: boolean;
+};
+
 // Navigation links
-export const NAV_LINKS = [
-    { href: "/", label: "Home" },
+export const NAV_LINKS: NavLink[] = [
+    { href: "/", label: "Home", drawer_only: true },
     { href: "/posts", label: "Blog" },
     { href: "/tags", label: "Tags" },
     { href: "/channel", label: "Channel" },
     { href: "/friends", label: "Links" },
     { href: "/about", label: "About" },
-] as const;
+];
