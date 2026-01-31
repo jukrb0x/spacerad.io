73ed3b238ac7a0b3018573bb475170d5301023f6
Jabriel <jabrielah@outlook.com>
2026-01-31 23:39:49 +0800

    fix: minor changes

--- src/consts.ts
+++ src/consts.ts
@@ -1,9 +1,9 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Spacerad.io";
+export const SITE_TITLE = "Spacerad.io by Jabriel";
 export const SITE_TITLE_ALT = ["太空来电", "宇宙频道", "星际广播", "I'm Jabriel", "智人大冲浪"] as const;
-export const SITE_DESCRIPTION = "Personal blog and digital garden";
+export const SITE_DESCRIPTION = "Space Radio by Jabriel, another random blog.";
 export const SITE_SOCIAL_IMAGE = "/og-image.png";
 export const DEFAULT_POST_SOCIAL_IMAGE = "/og-image.png";
 
