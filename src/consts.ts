cd2a8d7d17c1a74641a9289b4d0c5fb7fdbfab3a
Jabriel <jabrielah@outlook.com>
2026-02-01 01:08:19 +0800

    fix: minor changes

--- src/consts.ts
+++ src/consts.ts
@@ -4,7 +4,7 @@
 export const SITE_TITLE = "Spacerad.io";
 export const SITE_TITLE_OG = "Spacerad.io · On air with Jabriel";
 export const SITE_TITLE_ALT = ["太空来电", "宇宙频道", "星际广播", "I'm Jabriel", "智人大冲浪"] as const;
-export const SITE_DESCRIPTION = "Space Radio, another random blog. I am the host, Jabriel. Sharing my thoughts on game development, web technology, and other interesting stuff.";
+export const SITE_DESCRIPTION = "Space Radio, another random blog. Sharing my thoughts on game development, web technology, and other interesting stuff.";
 export const SITE_SOCIAL_IMAGE = "/og-image.png";
 export const DEFAULT_POST_SOCIAL_IMAGE = "/placeholder.png";
 
